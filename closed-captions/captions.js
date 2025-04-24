bb8f46d3fb881bd162b75fcda4f9d555baab0afa
Ufuk <hibbayt@gmail.com>
2025-04-24 03:22:39 +0200

    finishing touches, uitbreiding beatpulse, & fixes voor captions

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -3,21 +3,32 @@ captions[1] = ['4.5', '6.2'];
 captions[2] = ['6.3', '9.5'];
 //And blood-black nothingness began to spin
 captions[3] = ['10', '14.5'];
-captions[4] = ['18', '21'];
-captions[5] = ['31', '33'];
+
+captions[4] = ['15', '18'];
+
+captions[5] = ['18', '21'];
+captions[6] = ['31', '33'];
 //Fuck off, skin-job!
-captions[6] = ['34', '37.3'];
+captions[7] = ['34', '37.3'];
 
-captions[7] = ['37.3', '42'];
-captions[8] = ['42.5', '46'];
-captions[9] = ['46.5', '49.5'];
+captions[8] = ['37.3', '42'];
+captions[9] = ['42.5', '46'];
+captions[10] = ['46.5', '49.5'];
 //Have you ever been in an institution?
-captions[10] = ['48', '52'];
+captions[11] = ['48', '52'];
 
-captions[11] = ['62', '65'];
-captions[12] = ['71', '73'];
-captions[13] = ['73', '78'];
+captions[12] = ['62', '65'];
+captions[13] = ['71', '73'];
+captions[14] = ['73', '78'];
 //Do they keep you in a cell?
-captions[14] = ['79', '80.5'];
-captions[15] = ['82.5', '85.5'];
-captions[16] = ['140', '143'];
+captions[15] = ['79', '80.5'];
+captions[16] = ['82.5', '85.5'];
+captions[17] = ['86', '88'];
+
+captions[18] = ['110', '115'];
+
+
+captions[19] = ['140', '144'];
+captions[20] = ['143', '146.5'];
+captions[21] = ['146.5', '148'];
+
